008fe1e0ad93ad2fbd4c64a6a9f0167d8cb4575c
Michael Flanagan <micflan@gmail.com>
2013-04-18 21:20:55 +0200

    adding post_link to post data for use in templates

--- system/SlackLoader.php
+++ system/SlackLoader.php
@@ -88,6 +88,7 @@ class SlackLoader
             $data = array_merge($this->pages['_post'], $cleanPage, json_decode($json, true));
 
             $data['category_text'] = implode(', ', $data['categories']);
+            $data['post_link'] = $this->config['posts_url_prefix'] . $uid;
 
             if ($tmpl === true) {
                 // Set template variables
